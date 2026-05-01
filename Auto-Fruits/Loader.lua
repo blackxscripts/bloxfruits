db3bb63d93b87d9fafeafea9397cf294be72d4c0
BLACK X SCRIPTS <blackxscripts07@gmail.com>
2026-05-01 10:21:21 -0300

    Set up loader with module initializations
    Initialize global error handling and start the main loop with required modules.

--- Auto-Fruits/Loader.lua
+++ Auto-Fruits/Loader.lua
@@ -1,1 +1,16 @@
+--// LOADER
 
+local Modules = script:WaitForChild("Modules")
+
+local Team = require(Modules.Team)
+local Collect = require(Modules.Collect)
+local Store = require(Modules.Store)
+local ServerHop = require(Modules.ServerHop)
+local Rejoin = require(Modules.Rejoin)
+local ErrorHandler = require(Modules.ErrorHandler)
+local GlobalError = require(Modules.GlobalError)
+local MainLoop = require(Modules.MainLoop)
+
+-- Inicializações
+GlobalError.Init(Rejoin)
+MainLoop.Start(Team, Collect, Store, ServerHop, ErrorHandler)
